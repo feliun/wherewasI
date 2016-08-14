b4710a694a0c34974314954b01453b3f97eb99de
feliun <felipe.polo@gmail.com>
2016-08-14 16:55:05 +0100

    Displaying a simple map

---
+++ Where Was I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where Was I
+//
+//  Created by Gibbonsoft Limited on 14/08/2016.
+//  Copyright © 2016 feliun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
